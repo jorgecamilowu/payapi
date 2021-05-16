e6c986ddcf9ed0deae5b5387828b0ef57dc3cc26
Jorge <jwu@assanet.com>
2021-05-16 00:39:01 -0500

    Sets storybook to use material ui theme

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,32 @@
+import React from 'react';
+import * as nextImage from 'next/image';
+import { ThemeProvider, CssBaseline } from '@material-ui/core';
+import { addDecorator } from '@storybook/react';
+import { createMuiTheme } from '@material-ui/core/styles';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+// workaround to make storybook compatible with next/image
+Object.defineProperty(nextImage, 'default', {
+  configurable: true,
+  value: (props) => {
+    return <img {...props} />;
+  },
+});
+
+const Center = ({ children }) => (
+  <ThemeProvider theme={createMuiTheme()}>
+    <CssBaseline />
+    <div style={{ padding: '20px' }}>{children}</div>
+  </ThemeProvider>
+);
+
+addDecorator((storyFn) => <Center>{storyFn()}</Center>);
